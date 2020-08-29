efa030564e4089cc347384a40eb906cbb206e355
donzil <firstmanfallen@hotmail.com>
2020-08-29 02:06:59 +0100

    revised EntityDrops

--- src/java/com/drzenovka/leatherexpanded/init/ModItems.java
+++ src/java/com/drzenovka/leatherexpanded/init/ModItems.java
@@ -1,18 +1,14 @@
 package com.drzenovka.leatherexpanded.init;
 
-
-//import com.drzenovka.leatherexpanded.item.CowHide;
 import com.drzenovka.leatherexpanded.item.PeltCreator;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
-public class ModItems {
-	
-	//public static final CowHide cowHide = new CowHide();
-	public static final PeltCreator peltCreator = new PeltCreator();
-	
-	public static final void init() {
-		//GameRegistry.registerItem(cowHide, "cowHide");
+public class ModItems 
+{
+	public static final PeltCreator peltCreator = new PeltCreator();	
+	public static final void init() 
+	{
 		GameRegistry.registerItem(peltCreator, "peltCreator");
 	}
 
